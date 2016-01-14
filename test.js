882048426ae2740c8493e804975f3e26346633f7
srph <seraphipod@gmail.com>
2016-01-15 04:45:46 +0800

    Add test case for `setContext`.
    It also fails.

--- test.js
+++ test.js
@@ -12,11 +12,19 @@ MyComponent.contextTypes = {
 };
 
 describe('sample test', () => {
-  it('should succeed', () => {
+  it('setProps: should succeed', () => {
     const wrapper = shallow(<MyComponent x={5} />, { context: { location: { pathname: 'x' }} });
     expect(wrapper.first('div').text()).to.equal('x');
 
     wrapper.setProps({ x: 5 });
+    expect(wrapper.first('div').text()).to.equal('x');
+  });
+
+  it('setContext: should succeed', () => {
+    const wrapper = shallow(<MyComponent x={5} />, { context: { location: { pathname: 'x' }} });
+    expect(wrapper.first('div').text()).to.equal('x');
+
+    wrapper.setContext({ x: 5 });
     expect(wrapper.first('div').text()).to.equal('x');
   });
 });
